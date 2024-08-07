3aca26924230199ebbb24d9094f426a4247b5da0
raine-lemon <raine@lemoncloud.io>
2024-08-07 16:29:06 +0900

    chore: update version - 최소 버전 수정

--- buildSystem/src/main/kotlin/io/lemon/android/buildSystem/Config.kt
+++ buildSystem/src/main/kotlin/io/lemon/android/buildSystem/Config.kt
@@ -9,7 +9,7 @@ import java.time.format.DateTimeFormatter
 object Config {
 
     const val COMPILE_SDK = 34
-    const val MIN_SDK = 26
+    const val MIN_SDK = 24
     const val TARGET_SDK = 34
     const val VERSION_NAME = "0.0.1"
     val VERSION_CODE: Int =
