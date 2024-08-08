21776a3a420b357a2030308f6da42a1d9f4200fd
raine-lemon <raine@lemoncloud.io>
2024-08-08 14:21:59 +0900

    chore: remove unnecessary code

--- buildSystem/build.gradle.kts
+++ buildSystem/build.gradle.kts
@@ -1,17 +1,6 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     `kotlin-dsl`
-}
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_17)
-    }
 }
 
 dependencies {
